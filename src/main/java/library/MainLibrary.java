4db07b0a45f726f759ff63fa5378d528516c13f1
Yan Zhou <yanzhou@MacBook-Pro-de-Yan.local>
2022-03-29 20:35:01 -0400

    ajouter les attributs avec @Autowired

--- src/main/java/library/MainLibrary.java
+++ src/main/java/library/MainLibrary.java
@@ -1,5 +1,9 @@
 package library;
 
+import library.model.*;
+import library.repository.*;
+import library.service.ServiceLibrary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +11,33 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainLibrary implements CommandLineRunner {
 
+    @Autowired
+    private ServiceLibrary serviceLibrary;
+
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @Autowired
+    private LibraryUserRepository libraryUserRepository;
+
+    @Autowired
+    private ExemplaireRepository exemplaireRepository;
+
+    @Autowired
+    private EmpruntRepository empruntRepository;
+
+    @Autowired
+    private AmendeRepository amendeRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(MainLibrary.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+
+
 
     }
 }
--- src/main/java/library/repository/AmendeRepository.java
+++ src/main/java/library/repository/AmendeRepository.java
@@ -1,4 +1,7 @@
 package library.repository;
 
-public interface AmendeRepository {
+import library.model.Amende;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AmendeRepository extends JpaRepository<Amende, Long> {
 }
