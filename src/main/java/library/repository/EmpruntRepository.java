2d40160f64ce5107376c03c14a3e9812eda03145
Yan Zhou <yanzhou@MacBook-Pro-de-Yan.local>
2022-04-04 19:50:24 -0400

    ajouter une fonction findEmpruntById dans empruntRepository

--- src/main/java/library/repository/EmpruntRepository.java
+++ src/main/java/library/repository/EmpruntRepository.java
@@ -2,6 +2,12 @@ package library.repository;
 
 import library.model.Emprunt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface EmpruntRepository extends JpaRepository<Emprunt,Long> {
+    @Query("select e from Emprunt e where e.id = :empruntId")
+    Optional<Emprunt> findEmpruntById(@Param("empruntId") long empruntId);
 }
